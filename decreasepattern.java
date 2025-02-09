127dfd70e033350e12bce006064f893ebbba6137
susenther <132448488+Susenther@users.noreply.github.com>
2025-02-09 09:21:57 +0530

    commit

---
+++ decreasepattern.java
@@ -0,0 +1,13 @@
+import java.util.*;
+  public class decreasepattern{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in):
+        int n = sc.nextint();
+        for(int i =1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                system.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+  }
