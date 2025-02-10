e734fcda347fbaa225b02a8df59fc7e04d571d09
susenther <132448488+Susenther@users.noreply.github.com>
2025-02-10 21:15:39 +0530

    commit

--- rightside.java
+++ rightside.java
@@ -4,7 +4,7 @@ import java.util.*;
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         for(int i =1;i<=n;i++){
-            for(j=i;j<=n;j++){
+            for(j=1;j<=i;j++){
             System.out.print(" ");
             }
             for(j=1;j<=n;j++){
