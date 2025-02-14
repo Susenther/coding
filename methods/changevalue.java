95b846e011d72b325765a6bc99221b06094c19b2
susenther <132448488+Susenther@users.noreply.github.com>
2025-02-14 10:42:10 +0530

    commit

--- methods/changevalue.java
+++ methods/changevalue.java
@@ -2,6 +2,11 @@ import java.util.*;
     public class changevalue{
         public static void main(String [] args){
             int []arr = {0,10,15,20,25}
-            
+            change(arr);
+            System.out.print(Arrays.toString(arr));
         }
     }
+
+        static change(int [] nums){
+            nums [0] = 25
+        }
