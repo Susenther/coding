66c5ddecd5eb3e4813cab09f5bd0431317beaa86
susenther <132448488+Susenther@users.noreply.github.com>
2025-02-19 11:13:21 +0530

    commit

--- methods/sum.java
+++ methods/sum.java
@@ -6,7 +6,7 @@ public class sum {
         if(n==0)
         return 0;
         else
-        return n + (n-1); 
+        return n + sum(n-1); 
     }
     public static void main(String [] args){
         Scanner ip = new Scanner(System.in);
