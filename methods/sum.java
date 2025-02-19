9b00ca74afd8f1dad5756d5c1e15f1a77d1d6093
susenther <132448488+Susenther@users.noreply.github.com>
2025-02-19 11:05:44 +0530

    commit

---
+++ methods/sum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class sum {
+    static int sum(int n){
+        if(n==1)
+        return 1;
+        else
+        return n + (n-1); 
+    }
+    public static void main(String [] args){
+        Scanner ip = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n = ip.nextInt();
+        int res = sum(n);
+        System.out.println("The sum is:" +res);
+
+    }
+}
+    
+
