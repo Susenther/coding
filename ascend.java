5c11f0b6ad571b15867e02c05d27e3090b85d9f6
Your Name <your.email@example.com>
2025-02-24 11:42:28 +0530

    commit

---
+++ ascend.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class ascend {
+   
+    
+        public static boolean isSorted(int []a,int n ){
+            boolean asc = true,dec = true;
+            for(int i=0;i<n-1;i++)
+            {
+                if(a[i]<a[i+1])
+                asc =  false;
+                if(a[i]>a[i+1])
+                dec = false;
+                
+            }
+            return asc || dec;
+    
+        }
+    
+        public static void main(String []  args){
+    
+        Scanner ip = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n =ip.nextInt();
+        int []arr = new int[n];
+        System.out.println("Enter the Elements: ");
+        for(int i=0;i<n;i++)
+           arr[i] = ip.nextInt();
+    
+           if(isSorted(arr,n))
+                  System.out.print("yes! Sorted");
+    
+            else
+               System.out.print("no! Not Sorted");      
+        
+    
+    
+        }
+    
+        
+    }
+    
+    
+
