0a7f0c46397d448520e50574ea66b2520f475ac2
susenther <132448488+Susenther@users.noreply.github.com>
2025-02-24 20:13:25 +0530

    commit

---
+++ Getclass.java
@@ -0,0 +1,9 @@
+public class getclass {
+    public static void main(String [] args){
+        int arr[] = {1,2,3,4,5};
+        System.out.println(arr);
+        System.out.println(arr.getClass());
+        System.out.println(arr.getClass().getSuperclass());
+    }
+    
+}
